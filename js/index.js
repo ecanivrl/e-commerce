c43ec9dfa9e60e3ebfc724110be04c3b27c72331
Ercan <kadri503394@gmail.com>
2023-01-12 15:57:47 +0300

    feat: added javascript operation for mobile menu

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const btnOpenSidebar = document.querySelector("#btn-menu")
+const sidebar = document.querySelector("#sidebar")
+const btnCloseSidebar = document.querySelector("#close-sidebar")
+
+btnOpenSidebar.addEventListener("click", function(){
+    sidebar.style.left = "0"
+})
+
+btnCloseSidebar.addEventListener("click", function(){
+    sidebar.style.left = "-100%"
+})
