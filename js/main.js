aca5a65b215d6e862db5c94246e62d33e032b31c
Ercan <kadri503394@gmail.com>
2023-01-13 17:31:25 +0300

    fix: product editing

--- js/main.js
+++ js/main.js
@@ -2,26 +2,16 @@
 import headerFunc from './header.js';
 import productsFunc from "./products.js";
 
-
-
 // !Add product to local storage start
-async function getData() {
-  const photos = await fetch('../js/data.json');
+(async function () {
+  const photos = await fetch("../js/data.json");
   const data = await photos.json();
-
-  data ? localStorage.setItem('products', JSON.stringify(data)) : []
-
-}
-
-getData();
-
-const products = localStorage.getItem("products")
-
+  data ? localStorage.setItem("products", JSON.stringify(data)) : [];
+  productsFunc();
+})();
 // !Add product to local storage end
 
 // !Add product to CART local storage Start
  const cartItems = document.querySelector(".header-cart-count");
-
 cartItems.innerHTML = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")).length : "0";
-
 // !Add product to CART local storage end
