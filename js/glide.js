764a0f9ae2495a32bc37f9a63ada1fb975863157
Ercan <kadri503394@gmail.com>
2023-01-13 11:08:54 +0300

    feat: added carousel slider cdn

--- js/glide.js
+++ js/glide.js
@@ -1,1 +1,43 @@
-new Glide('.glide').mount()
+const config = {
+    type: "carousel",
+    gap:20,
+    autoplay: 3000,
+    perView: 4,
+    hoverpause: true,
+    breakpoints: {
+        1200: {
+            perView: 3,
+        },
+        992: { 
+            perView: 2,
+         },
+        768: {
+            perView: 1,
+        },
+    },
+    
+}
+
+new Glide(".product-carousel", config).mount();
+
+const config2 = {
+    type: "carousel",
+    gap:20,
+    autoplay: 3000,
+    perView: 4,
+    hoverpause: true,
+    breakpoints: {
+        1200: {
+            perView: 3,
+        },
+        992: { 
+            perView: 2,
+         },
+        768: {
+            perView: 1,
+        },
+    },
+    
+}
+
+new Glide(".product-carousel2", config2).mount();
