288ba1cc86d31241ba42d6aed7ccb1b1dacb07a6
Ercan <kadri503394@gmail.com>
2023-01-13 11:36:48 +0300

    feat: added product to localStorage

--- js/index.js
+++ js/index.js
@@ -99,3 +99,18 @@ function showSlides(n) {
 }
 
 //! slider end
+
+// !Add product to local storage start
+async function getData(){
+  const photos = await fetch('../js/data.json');
+  const data = await photos.json();
+
+  data ?  localStorage.setItem('products', JSON.stringify(data)) : []
+
+}
+
+getData();
+
+const products = localStorage.getItem("products")
+
+// !Add product to local storage end
