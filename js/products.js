f68e526fb6528d9712035705acabaf67c33b5c61
Ercan <kadri503394@gmail.com>
2023-01-16 11:46:17 +0300

    feat: refactoring and completed project

--- js/products.js
+++ js/products.js
@@ -1,48 +1,45 @@
 import { product1 } from "./glide.js";
 
-
-let products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")): [];
 let cart = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : [];
 
-
-
-function addToCart() {
+function addToCart(products) {
   const cartItems = document.querySelector(".header-cart-count");
   const buttons = [...document.getElementsByClassName("add-to-cart")];
   buttons.forEach((button) => {
     const inCart = cart.find((item) => item.id === Number(button.dataset.id));
     if (inCart) {
-      button.setAttribute("disabled", "disabled")
-    }else{
- button.addEventListener("click", function (e) {
-      e.preventDefault();
-      const id = e.target.dataset.id;
-      const findProduct = products.find((product) => product.id === Number(id));
-      cart.push({ ...findProduct, quantity: 1 });
-      localStorage.setItem("cart", JSON.stringify(cart));
+      button.setAttribute("disabled", "disabled");
+    } else {
+      button.addEventListener("click", function (e) {
+        e.preventDefault();
+        const id = e.target.dataset.id;
+        const findProduct = products.find(
+          (product) => product.id === Number(id)
+        );
+        cart.push({ ...findProduct, quantity: 1 });
+        localStorage.setItem("cart", JSON.stringify(cart));
         button.setAttribute("disabled", "disabled");
         cartItems.innerHTML = cart.length;
-    });
+      });
     }
-   
   });
 }
 
-function productRoutes() {
+function productRoute() {
   const productLink = document.getElementsByClassName("product-link");
- Array.from(productLink).forEach((button) => {
-  button.addEventListener("click", function(e){
-    e.preventDefault()
-    const id = e.target.dataset.id;
-    localStorage.setItem("productId", JSON.stringify(id));
-    window.location.href = "single-product.html";
-  })
- })
+  Array.from(productLink).forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+      const id = e.target.dataset.id;
+      localStorage.setItem("productId", JSON.stringify(id));
+      window.location.href = "single-product.html";
+    });
+  });
 }
 
-function productsFunc() {
+function productsFunc(products) {
   const productsContainer = document.getElementById("product-list");
 
   let results = "";
@@ -78,7 +75,7 @@ function productsFunc() {
           <strong class="new-price">$${item.price.newPrice.toFixed(2)}</strong>
           <span class="old-price">$${item.price.oldPrice.toFixed(2)}</span>
         </div>
-        <span class="product-discount">--${item.discount}%</span>
+        <span class="product-discount">-${item.discount}%</span>
         <div class="product-links">
           <button class="add-to-cart" data-id=${item.id}>
             <i class="bi bi-basket-fill"></i>
@@ -86,7 +83,7 @@ function productsFunc() {
           <button>
             <i class="bi bi-heart-fill"></i>
           </button>
-          <a href="#" class="product-link" data-id="${item.id}">
+          <a href="#" class="product-link" data-id=${item.id}>
             <i class="bi bi-eye-fill"></i>
           </a>
           <a href="#">
@@ -96,12 +93,11 @@ function productsFunc() {
       </div>
     </li>
     `;
-    productsContainer ?  productsContainer.innerHTML = results: "";
-  addToCart();
-
+    productsContainer ? (productsContainer.innerHTML = results) : "";
+    addToCart(products);
   });
   product1();
-  productRoutes();
+  productRoute();
 }
 
 export default productsFunc;
